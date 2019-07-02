6c3e04a27b30b409a920dca14e96e9c41017e534
tegner <jtegner@gmail.com>
2019-07-02 09:07:05 +0200

    back to const

--- dist/eb-colors.ts
+++ dist/eb-colors.ts
@@ -4,12 +4,12 @@ export const Bordeaux = '#8a0c36';export const Reddark = '#900';export const Red
  */
 function hexToRgb(hex: string) {
   // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
   hex = hex.replace(shorthandRegex, function(m, r, g, b) {
     return r + r + g + g + b + b;
   });
 
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
   return result
     ? {
         r: parseInt(result[1], 16),
